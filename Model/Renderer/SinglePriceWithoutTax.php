cb13a2eddf8a2a7443202c3aed6f4c8842f34c29
Lukas Lauser <lukas.lauser@sovendus.de>
2024-03-19 16:39:19 +0100

    1.0.0

--- Model/Renderer/SinglePriceWithoutTax.php
+++ Model/Renderer/SinglePriceWithoutTax.php
@@ -2,12 +2,10 @@
 
 namespace LauserMedia\CustomPrice\Model\Renderer;
 
-use Magento\Framework\Exception\NoSuchEntityException;
-
 class SinglePriceWithoutTax extends \Magetrend\PdfTemplates\Model\Pdf\Element\Items\Column\DefaultRenderer
 {
+
     public $productRepository;
-    protected $taxHelper;
 
     public function __construct(
         \Magetrend\PdfTemplates\Helper\Data $moduleHelper,
@@ -15,27 +13,22 @@ class SinglePriceWithoutTax extends \Magetrend\PdfTemplates\Model\Pdf\Element\It
         \Magetrend\PdfTemplates\Model\Pdf\Decorator $decorator,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        \Magento\Tax\Helper\Data $taxHelper,
         array $data = []
     ) {
-        $this->taxHelper = $taxHelper;
         $this->productRepository = $productRepository;
         parent::__construct($moduleHelper, $element, $decorator, $scopeConfig, $data);
     }
 
     public function getRowValue($item = null)
     {
-        if ($item === null || !$item->getProductId()) {
-            return '';
-        }
-
         try {
             $product = $this->productRepository->getById($item->getProductId());
-            $netPrice = $this->taxHelper->getPrice($product, $product->getFinalPrice(), false );
         } catch (NoSuchEntityException $e) {
             return '';
         }
 
-        return number_format($netPrice, 2, '.', '') . ' ' . $item->getOrder()->getOrderCurrencyCode();
+        $netPrice = $product->getPrice();
+
+        return number_format($netPrice, 2, '.', '') . ' ' . $product->getOrder()->getOrderCurrencyCode();
     }
 }
--- Model/Renderer/TotalPriceWithoutTax.php
+++ Model/Renderer/TotalPriceWithoutTax.php
@@ -2,12 +2,9 @@
 
 namespace LauserMedia\CustomPrice\Model\Renderer;
 
-use Magento\Framework\Exception\NoSuchEntityException;
-
 class TotalPriceWithoutTax extends \Magetrend\PdfTemplates\Model\Pdf\Element\Items\Column\DefaultRenderer
 {
     public $productRepository;
-    protected $taxHelper;
 
     public function __construct(
         \Magetrend\PdfTemplates\Helper\Data $moduleHelper,
@@ -15,30 +12,23 @@ class TotalPriceWithoutTax extends \Magetrend\PdfTemplates\Model\Pdf\Element\Ite
         \Magetrend\PdfTemplates\Model\Pdf\Decorator $decorator,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        \Magento\Tax\Helper\Data $taxHelper,
         array $data = []
     ) {
-        $this->taxHelper = $taxHelper;
         $this->productRepository = $productRepository;
         parent::__construct($moduleHelper, $element, $decorator, $scopeConfig, $data);
     }
 
     public function getRowValue($item = null)
     {
-        if ($item === null || !$item->getProductId()) {
-            return '';
-        }
-
-        $qtyOrdered = $item->getQtyOrdered();
-
         try {
             $product = $this->productRepository->getById($item->getProductId());
-            $netPrice = $this->taxHelper->getPrice($product, $product->getFinalPrice(), false );
-            $totalNetPrice = $netPrice * $qtyOrdered;
         } catch (NoSuchEntityException $e) {
             return '';
         }
 
-        return number_format($totalNetPrice, 2, '.', '') . ' ' . $item->getOrder()->getOrderCurrencyCode();
+        $netPrice = $product->getPrice();
+        $netTotal = $netPrice * $product->getQtyOrdered();
+
+        return number_format($netTotal, 2, '.', '') . ' ' . $item->getOrder()->getOrderCurrencyCode();
     }
 }
